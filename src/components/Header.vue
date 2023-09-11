1c5b5502f1129f1b67308fa6be1e2fb6d8c5e387
JasurAvazov <jasuravazov4@gmail.com>
2023-09-11 21:52:15 +0500

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<header class="header">
+        <div class="header__top">
+            <div class="container">
+                <p>dsadsadsa</p>
+            </div>
+        </div>
+        <div class="header__bot">
+            <p>sdasdasda</p>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.header{
+    &__top{
+        background-color: #202020;
+    }
+    .container{
+        padding: 25px 15px;
+    }
+}
+</style>
